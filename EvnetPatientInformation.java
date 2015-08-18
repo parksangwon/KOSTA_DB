f15ab14c8620675163d660e591d03ee998419cb1
parksangwon <ourtree4@gmail.com>
2015-08-18 15:22:19 +0900

    다정
    다정

--- EvnetPatientInformation.java
+++ EvnetPatientInformation.java
@@ -12,21 +12,8 @@ public class EvnetPatientInformation implements ActionListener {
 
 		String action = ae.getActionCommand().trim();
 
-		if(action.equals("추가")){
-			kh.display.editClear();
-			kh.changeStatus(true);
-			kh.nameTextField.requestFocus();
-		}
-		else if(action.equals("조회")){
-		}
-		else if(action.equals("수정")){
-			kh.changeStatus(true);
-			kh.nameTextField.selectAll();
-			kh.nameTextField.requestFocus();
-		}
-		else if(action.equals("삭제")){
-		}
-		else if(action.equals("불러오기")){
+	
+		if(action.equals("불러오기")){
 		}
 		else if(action.equals("  삭제  ")){ // 이미지 삭제
 		}
