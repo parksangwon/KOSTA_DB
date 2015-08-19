c79553eb63d1e2f1f78f4b88e7f1f6968f36c783
DajeongKim <coco7144@naver.com>
2015-08-19 11:23:20 +0900

    다정에이스
    ㅇㅇ

--- EvnetPatientInformation.java
+++ EvnetPatientInformation.java
@@ -1,6 +1,7 @@
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
+import java.util.*;
 
 public class EvnetPatientInformation implements ActionListener {
 
@@ -39,6 +40,71 @@ public class EvnetPatientInformation implements ActionListener {
 			kh.display.postCodeClear();
 		}
 		else if(action.equals("확인")){
+			String birth = kh.birthTextField.getText();
+			String phone = kh.phoneTextField.getText();
+			
+			if ((kh.nameTextField.getText()).equals(""))
+			{
+				JOptionPane.showMessageDialog(kh, "이름을 입력해 주세요.", "이름 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			else if(kh.genderComboBox.getSelectedIndex() == 0)
+			{
+				JOptionPane.showMessageDialog(kh, "성별을 입력해 주세요.", "성별 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			else if(kh.birthTextField.getText().equals(""))
+			{
+				JOptionPane.showMessageDialog(kh, "생년월일을 입력해 주세요.", "생년월일 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			else if(kh.subjectComboBox.getSelectedIndex() == 0)
+			{
+				JOptionPane.showMessageDialog(kh, "진료과목을 입력해 주세요.", "진료과 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			else if(kh.phoneTextField.getText().equals(""))
+			{
+				JOptionPane.showMessageDialog(kh, "전화번호를 입력해 주세요.", "전화번호 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			else if(kh.addrNumberTextField.getText().equals("") || kh.addrTextField.getText().equals("") || kh.addrDetailTextField.getText().equals(""))
+			{
+				JOptionPane.showMessageDialog(kh, "주소를 입력해 주세요.", "주소 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			// 생년월일 validation
+			if (birth.length() != 6)
+			{
+				JOptionPane.showMessageDialog(kh, "생년월일을 yymmdd형으로 입력해 주세요", "생년월일 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			for (int i =0; i< birth.length() ; i++ )
+			{
+				char a = birth.charAt(i);
+				if (!(a>=48 && a<=57))
+				{
+					JOptionPane.showMessageDialog(kh, "숫자만 입력해 주세요.", "생년월일 오류", JOptionPane.WARNING_MESSAGE);
+					return;
+				}	
+			}
+			// 전화번호 validation
+			if (phone.length() != 11)
+			{
+				JOptionPane.showMessageDialog(kh, "전화번호 11자리를 입력해 주세요", "전화번호 오류", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			for (int i =0; i< phone.length() ; i++ )
+			{
+				char a = phone.charAt(i);
+				if (!(a>=48 && a<=57))
+				{
+					JOptionPane.showMessageDialog(kh, "숫자만 입력해 주세요.", "전화번호 오류", JOptionPane.WARNING_MESSAGE);
+					return;
+				}	
+			}
+
+			// DB에 저장
 			kh.function = "Normal";
 			kh.display.setButton(kh.function);
 			kh.changeStatus(false);
